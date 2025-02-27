ca74b923f1ba6f7918cabae5e119b9bfe0ee0a89
Zach <zachauten@gmail.com>
2025-02-26 21:39:09 -0500

    Update flake.nix

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -1,29 +1,67 @@
 {
-  description = "Basic Nix Flake for macOS with a GUI app and dev shell config";
+  description = "Example nix-darwin system flake, place in /etc/nix-darwin";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";  # You can use other versions as needed
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, ... }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-darwin";
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =  with pkgs;
+        [
+          cargo
+          deno
+          docker
+          dprint
+          ffmpeg
+          flyctl
+          gh
+          gnupg1compat
+          gnuplot
+          imagemagick
+          jdk
+          jq
+          maven
+          neovim
+          nodejs
+          opentofu
+          pandoc
+          pinentry-tty
+          qrencode
+          tmux
+          tree
+          vhs
+          vim
+          yt-dlp
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
     };
-  in {
-
-    devShells.x86_64-darwin.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        git
-        docker
-        gnupg
-        gh
-        mysql84
-
-        # Languages
-        deno
-        jdk
-        maven
-      ];
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#macbook
+    darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
     };
   };
 }
