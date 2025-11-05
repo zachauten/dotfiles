2302ea2266fcbd8ae90f84ffdea163e49e7b8765
Zach Auten <zauten@bandwidth.com>
2025-11-05 14:00:57 -0500

    update flake.nix and add nix.conf for darwin

--- nix-darwin/work/flake.nix
+++ nix-darwin/work/flake.nix
@@ -1,61 +1,69 @@
 {
   description = "Example nix-darwin system flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
-    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    
+    typespec-fix.url = "github:NixOS/nixpkgs/829f8bb29dad98a6af914cd3182bcd6dd4e1f1cd";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, typespec-fix }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = with pkgs;
-        [
-          vim
-          neovim
-          git
-          tree
-          docker
-          gnupg
-          gh
-          mysql84
-          dprint
-          awscli2
-          jq
-          pinentry-tty
-          jupyter
+    configuration = { pkgs, ... }:
 
-          # Languages
-          deno
-          maven
-          python313
-          nodejs_20
-        ];
-      environment.variables = { 
-        JAVA_HOME = "${pkgs.openjdk}";
-      };
-      # nix-darwin conflicts with the determinate systems nix
-      nix.enable = false;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
-    };
+     let
+       pkgs-typespec-fix = import typespec-fix {
+         system = pkgs.system;
+       };
+     in
+     {
+       # List packages installed in system profile. To search by name, run:
+       # $ nix-env -qaP | grep wget
+       environment.systemPackages = with pkgs;
+         [
+           vim
+           neovim
+           git
+           tree
+           docker
+           gnupg
+           gh
+           mysql84
+           dprint
+           awscli2
+           jq
+           pinentry-tty
+           jupyter
+           pkgs-typespec-fix.typespec
+ 
+           # Languages
+           deno
+           maven
+           python313
+           nodejs_20
+         ];
+       environment.variables = { 
+         JAVA_HOME = "${pkgs.openjdk}";
+       };
+       # nix-darwin conflicts with the determinate systems nix
+       nix.enable = false;
+ 
+       # Necessary for using flakes on this system.
+       nix.settings.experimental-features = "nix-command flakes";
+ 
+       # Enable alternative shell support in nix-darwin.
+       # programs.fish.enable = true;
+ 
+       # Set Git commit hash for darwin-version.
+       system.configurationRevision = self.rev or self.dirtyRev or null;
+ 
+       # Used for backwards compatibility, please read the changelog before changing.
+       # $ darwin-rebuild changelog
+       system.stateVersion = 5;
+ 
+       # The platform the configuration will be used on.
+       nixpkgs.hostPlatform = "x86_64-darwin";
+     };
   in
   {
     # Build darwin flake using:
